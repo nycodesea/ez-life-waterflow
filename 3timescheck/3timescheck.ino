9f8b877ed733c804ac1a15fc8f73ddb2fb910036
nycodesea <nyanko.shootingstar@gmail.com>
2026-06-22 11:14:59 +0900

    add 4days dry in a row watering and send moisture value every 10secs program

--- 3timescheck/3timescheck.ino
+++ 3timescheck/3timescheck.ino
@@ -1,38 +1,16 @@
-const int sensorPin = 35;  // 湿度センサー
-const int relayPin = 26;   // リレー
-int count =0;
+const int relayPin = 26;
 
 void setup() {
   Serial.begin(115200);
   pinMode(relayPin, OUTPUT);
-  digitalWrite(relayPin, LOW);
 }
 
 void loop() {
-  int moisture = analogRead(sensorPin);
+  Serial.println("HIGH");
+  digitalWrite(relayPin, HIGH);
+  delay(2000);
 
-  Serial.println(moisture);
-
-  if (moisture > 3000) {   // 乾燥判定（値は要調整）
-    count+=1;
-    Serial.println("Count up!");
-    Serial.println(count);
-  } else {
-    digitalWrite(relayPin, LOW);  // リレーOFF
-    count = 0;
-    Serial.println("Not dry.");
-  }
-
-  if (count >= 3) {   // 乾燥判定（値は要調整）
-      digitalWrite(relayPin, HIGH);   // リレーON
-      Serial.println("Turn on!!");
-      delay(5000);
-      count=0;
-      Serial.println("Count 0");
-      
-  }
-  
+  Serial.println("LOW");
   digitalWrite(relayPin, LOW);
-  Serial.println("Set low");
-  delay(60 * 1000);
+  delay(2000);
 }
---
+++ 4daysdry_print.ino
@@ -0,0 +1,105 @@
+//Wifi情報あり！！　注意！！
+#include <WiFi.h>
+#include <time.h>
+#include <WebServer.h>
+
+WebServer server(80);
+
+int currentMoisture = 0;
+
+const char* ssid = "Input SSID";
+const char* password = "Input password";
+
+const int sensorPin = 35;
+const int relayPin = 26;
+
+int dryDays = 0;
+int lastCheckedDay = -1;
+
+void handleRoot() {
+  String html = "";
+
+  html += "<h1>EZ Water Flow</h1>";
+  html += "<p>Moisture: " + String(currentMoisture) + "</p>";
+  html += "<p>Dry Days: " + String(dryDays) + "</p>";
+
+  server.send(200, "text/html", html);
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  configTime(9 * 3600, 0, "ntp.nict.jp", "pool.ntp.org");
+
+  server.on("/", handleRoot);
+  server.begin();
+
+  Serial.println(WiFi.localIP());
+
+  pinMode(relayPin, OUTPUT);
+
+  Serial.println("Watering!"); // 起動時水やり
+  digitalWrite(relayPin, HIGH);
+  delay(2000);
+  digitalWrite(relayPin, LOW);
+
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  struct tm timeinfo;
+  currentMoisture = analogRead(sensorPin);
+
+  if (getLocalTime(&timeinfo)) {
+
+    // 毎朝8:00に1回だけ実行
+    if (timeinfo.tm_hour == 8 &&
+        timeinfo.tm_min == 0 &&
+        timeinfo.tm_mday != lastCheckedDay) {
+
+      lastCheckedDay = timeinfo.tm_mday;
+
+      int total = 0;
+
+      for(int i = 0; i < 10; i++) {
+        total += analogRead(sensorPin);
+        delay(100);
+      }
+
+      int moisture = total / 10;
+      currentMoisture = moisture;
+
+      Serial.println("湿度チェック！");
+      Serial.print("Moisture: ");
+      Serial.println(moisture);
+
+      if (moisture > 3000) {
+        dryDays++;
+        Serial.print("Dry days: ");
+        Serial.println(dryDays);
+      } else {
+        dryDays = 0;
+        Serial.println("Not dry. Reset dryDays.");
+      }
+
+      if (dryDays > 3) {
+        Serial.println("Watering!");
+
+        digitalWrite(relayPin, HIGH);
+        delay(2000);
+        digitalWrite(relayPin, LOW);
+
+        dryDays = 0;
+        Serial.println("dryDays reset.");
+      }
+    }
+  }
+  server.handleClient();
+  delay(10*1000);
+}
